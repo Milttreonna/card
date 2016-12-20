cfcb3f8e05563fb47ae96095e621b89e4db3600d
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-12-20 09:33:46 -0600

    changing html name

--- calupdate.js
+++
@@ -1,9 +0,0 @@
-var options = {
-  access_token: 'TgQKNotRLzxq_FiC7Rw_g7zdYGZIlFJg',
-  tzid: 'Etc/UTC'
-};
-
-cronofy.listCalendars(options)
-  .then(function (response) {
-      var calendars = response.calendars;
-  });

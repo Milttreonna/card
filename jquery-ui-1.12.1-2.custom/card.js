b3867ebc295915cdbdb0d4939de3002196ead98a
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-12-21 11:32:07 -0600

    adding images and bootstrap

---
+++ jquery-ui-1.12.1-2.custom/card.js
@@ -0,0 +1,139 @@
+$('document').ready(function() {
+
+  $(function() {
+      $( "#resizable" ).resizable({
+ containment: "#container"
+ });
+      $( "#draggable" ).draggable({containment: "#container"});
+ });
+
+    $('#black').on('click', function() {
+        $('#card').css("background-color", "black");
+    });
+
+    $('#LIGHTCORAL').on('click', function() {
+        $('#card').css("background-color", "LIGHTCORAL");
+    });
+    $('#CRIMSON').on('click', function() {
+        $('#card').css("background-color", "CRIMSON");
+    });
+    $('#RED').on('click', function() {
+        $('#card').css("background-color", "RED");
+    });
+    $('#DARKRED').on('click', function() {
+        $('#card').css("background-color", "DARKRED");
+    });
+    $('#LIGHTPINK').on('click', function() {
+        $('#card').css("background-color", "LIGHTPINK");
+    });
+    $('#HOTPINK').on('click', function() {
+        $('#card').css("background-color", "HOTPINK");
+    });
+    $('#MEDIUMVIOLETRED').on('click', function() {
+        $('#card').css("background-color", "MEDIUMVIOLETRED");
+    });
+    $('#DEEPPINK').on('click', function() {
+        $('#card').css("background-color", "DEEPPINK");
+    });
+    $('#TOMATO').on('click', function() {
+        $('#card').css("background-color", "TOMATO");
+    });
+    $('#GOLD').on('click', function() {
+        $('#card').css("background-color", "GOLD");
+    });
+    $('#YELLOW').on('click', function() {
+        $('#card').css("background-color", "YELLOW");
+    });
+    $('#DARKKHAKI').on('click', function() {
+        $('#card').css("background-color", "DARKKHAKI");
+    });
+    $('#LAVENDER').on('click', function() {
+        $('#card').css("background-color", "LAVENDER");
+    });
+    $('#MEDIUMORCHID').on('click', function() {
+        $('#card').css("background-color", "MEDIUMORCHID");
+    });
+    $('#MEDIUMPURPLE').on('click', function() {
+        $('#card').css("background-color", "MEDIUMPURPLE");
+    });
+    $('#BLUEVIOLET').on('click', function() {
+        $('#card').css("background-color", "BLUEVIOLET");
+    });
+    $('#PURPLE').on('click', function() {
+        $('#card').css("background-color", "PURPLE");
+    });
+    $('#SLATEBLUE').on('click', function() {
+        $('#card').css("background-color", "SLATEBLUE");
+    });
+    $('#LIME').on('click', function() {
+        $('#card').css("background-color", "LIME");
+    });
+    $('#PALEGREEN').on('click', function() {
+        $('#card').css("background-color", "PALEGREEN");
+    });
+    $('#MEDIUMSPRINGGREEN').on('click', function() {
+        $('#card').css("background-color", "MEDIUMSPRINGGREEN");
+    });
+    $('#GREEN').on('click', function() {
+        $('#card').css("background-color", "GREEN");
+    });
+    $('#MEDIUMAQUAMARINE').on('click', function() {
+        $('#card').css("background-color", "MEDIUMAQUAMARINE");
+    });
+    $('#TEAL').on('click', function() {
+        $('#card').css("background-color", "TEAL");
+    });
+    $('#CYAN').on('click', function() {
+        $('#card').css("background-color", "CYAN");
+    });
+    $('#LIGHTBLUE').on('click', function() {
+        $('#card').css("background-color", "LIGHTBLUE");
+    });
+    $('#DODGERBLUE').on('click', function() {
+        $('#card').css("background-color", "DODGERBLUE");
+    });
+    $('#BLUE').on('click', function() {
+        $('#card').css("background-color", "BLUE");
+    });
+    $('#DARKBLUE').on('click', function() {
+        $('#card').css("background-color", "DARKBLUE");
+    });
+    $('#CHOCOLATE').on('click', function() {
+        $('#card').css("background-color", "CHOCOLATE");
+    });
+    $('#PERU').on('click', function() {
+        $('#card').css("background-color", "PERU");
+    });
+    $('#SADDLEBROWN').on('click', function() {
+        $('#card').css("background-color", "SADDLEBROWN");
+    });
+    $('#BROWN').on('click', function() {
+        $('#card').css("background-color", "BROWN");
+    });
+    $('#IVORY').on('click', function() {
+        $('#card').css("background-color", "IVORY");
+    });
+
+    $('#MISTYROSE').on('click', function() {
+        $('#card').css("background-color", "MISTYROSE");
+    });
+    $('#GRAY').on('click', function() {
+        $('#card').css("background-color", "GRAY");
+    });
+    $('#DARKGRAY').on('click', function() {
+        $('#card').css("background-color", "DARKGRAY");
+    });
+    $('#DARKSLATEGRAY').on('click', function() {
+        $('#card').css("background-color", "DARKSLATEGRAY");
+    });
+    $('#KHAKI').on('click', function() {
+        $('#card').css("background-color", "KHAKI");
+    });
+
+});
+//
+// $('button').on('click', function() {
+//   $('#textbox1').attr("disabled","disabled");
+//   $("#textbox1").draggable();
+//
+// });

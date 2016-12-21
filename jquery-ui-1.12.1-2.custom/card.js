56a561f5bc1f4a5d08a3dade37bfd3c3aa55b5d0
Milttreonna Owens <mowens@basecampcodingacademy.org>
2016-12-21 15:57:44 -0600

    added more functions and pics

--- jquery-ui-1.12.1-2.custom/card.js
+++ jquery-ui-1.12.1-2.custom/card.js
@@ -1,11 +1,236 @@
 $('document').ready(function() {
 
-  $(function() {
-      $( "#resizable" ).resizable({
- containment: "#container"
- });
-      $( "#draggable" ).draggable({containment: "#container"});
- });
+    //  $(function() {
+    //      $( "#resizable" ).resizable({
+    // containment: "#container"
+    // });
+    //      $( "#draggable" ).draggable({containment: "#container"});
+    // });
+
+    $("#thanks").on('click', function() {
+        $('<img src="tg/frame.png" style="height:400px; width:700px; position:relative;" id="picturess1">').appendTo(".stickers");
+        $('<img src="tg/frame3.png" style="height:400px; width:700px; position:relative;" id="picturess2">').appendTo(".stickers");
+        $('<img src="tg/gobble.png" style="height:200px; width:200px; position:relative;" id="picturess3">').appendTo(".stickers");
+        $('<img src="tg/indian.png" style="height:200px; width:200px; position:relative;" id="picturess4">').appendTo(".stickers");
+        $('<img src="tg/leaf.gif" style="height:200px; width:700px; position:relative;" id="picturess5">').appendTo(".stickers");
+        $('<img src="tg/leaf1.png" style="height:200px; width:700px; position:relative;" id="picturess6">').appendTo(".stickers");
+        $('<img src="tg/leaf4.png" style="height:200px; width:200px; position:relative;" id="picturess7">').appendTo(".stickers");
+        $('<img src="tg/leaves.gif" style="height:400px; width:700px; position:relative;" id="picturess8">').appendTo(".stickers");
+        $('<img src="tg/pump.png" style="height:200px; width:200px; position:relative;" id="picturess9">').appendTo(".stickers");
+        $('<img src="tg/tg.png" style="height:200px; width:200px; position:relative;" id="picturess10">').appendTo(".stickers");
+        $('<img src="tg/tg1.png" style="height:200px; width:200px; position:relative;" id="picturessr">').appendTo(".stickers");
+        $('<img src="tg/tg2.png" style="height:200px; width:200px; position:relative;" id="picturess11">').appendTo(".stickers");
+        $('<img src="tg/tg3.png" style="height:200px; width:200px; position:relative;" id="picturess12">').appendTo(".stickers");
+        $('<img src="tg/thanks4.png" style="height:200px; width:200px; position:relative;" id="picturess13">').appendTo(".stickers");
+        $('<img src="tg/thanks5.png" style="height:400px; width:700px; position:relative;" id="picturess14">').appendTo(".stickers");
+        $('<img src="tg/thanks6.png" style="height:200px; width:200px; position:relative;" id="picturess15">').appendTo(".stickers");
+        $('<img src="tg/turk.png" style="height:200px; width:200px; position:relative;" id="picturess16">').appendTo(".stickers");
+        $('<img src="tg/turk1.png" style="height:200px; width:200px; position:relative;" id="picturess17">').appendTo(".stickers");
+        $('<img src="tg/turk4.png" style="height:200px; width:200px; position:relative;" id="picturess18">').appendTo(".stickers");
+        $("#picturess1").draggable();
+        $("#picturess2").draggable();
+        $("#picturess3").draggable();
+        $("#picturess4").draggable();
+        $("#picturess5").draggable();
+        $("#picturess6").draggable();
+        $("#picturess7").draggable();
+        $("#picturess8").draggable();
+        $("#picturess9").draggable();
+        $("#picturess10").draggable();
+        $("#picturess11").draggable();
+        $("#picturess12").draggable();
+        $("#picturess13").draggable();
+        $("#picturess14").draggable();
+        $("#picturess15").draggable();
+        $("#picturess16").draggable();
+        $("#picturess17").draggable();
+        $("#picturess18").draggable();
+        $("#picturessr").draggable();
+
+
+
+    });
+
+
+    $("#bday").on('click', function() {
+        $('<img src="bday/bal.png" style="height:50px; width:50px; position:relative;" id="picturess19">').appendTo(".stickers");
+        $('<img src="bday/bal1.png" style="height:50px; width:50px; position:relative;" id="picturess20">').appendTo(".stickers");
+        $('<img src="bday/bal2.png" style="height:50px; width:50px; position:relative;" id="picturess21">').appendTo(".stickers");
+        $('<img src="bday/bal4.png" style="height:50px; width:50px; position:relative;" id="picturess22">').appendTo(".stickers");
+        $('<img src="bday/bday.png" style="height:50px; width:50px; position:relative;" id="picturess23">').appendTo(".stickers");
+        $('<img src="bday/bday1.png" style="height:50px; width:50px; position:relative;" id="picturess24">').appendTo(".stickers");
+        $('<img src="bday/cake.png" style="height:50px; width:50px; position:relative;" id="picturess25">').appendTo(".stickers");
+        $('<img src="bday/cake1.png" style="height:50px; width:50px; position:relative;" id="picturess26">').appendTo(".stickers");
+        $('<img src="bday/cake2.png" style="height:50px; width:50px; position:relative;" id="picturess27">').appendTo(".stickers");
+        $('<img src="bday/cake3.png" style="height:50px; width:50px; position:relative;" id="picturess28">').appendTo(".stickers");
+        $('<img src="bday/gifts.png" style="height:50px; width:50px; position:relative;" id="picturess29">').appendTo(".stickers");
+        $('<img src="bday/hat.png" style="height:50px; width:50px; position:relative;" id="picturess30">').appendTo(".stickers");
+        $('<img src="bday/stream.png" style="height:50px; width:50px; position:relative;" id="picturess31">').appendTo(".stickers");
+        $('<img src="bday/streamer2.png" style="height:50px; width:50px; position:relative;" id="picturess32">').appendTo(".stickers");
+        $('<img src="bday/streamers1.png" style="height:50px; width:50px; position:relative;" id="picturess33">').appendTo(".stickers");
+        $('<img src="bday/streamers4.png" style="height:50px; width:50px; position:relative;" id="picturess34">').appendTo(".stickers");
+        $('<img src="bday/streamers5.png" style="height:50px; width:50px; position:relative;" id="picturess35">').appendTo(".stickers");
+        $('<img src="bday/streamers6.png" style="height:50px; width:50px; position:relative;" id="picturess36">').appendTo(".stickers");
+        $("#picturess19").draggable();
+        $("#picturess20").draggable();
+        $("#picturess21").draggable();
+        $("#picturess22").draggable();
+        $("#picturess23").draggable();
+        $("#picturess24").draggable();
+        $("#picturess25").draggable();
+        $("#picturess26").draggable();
+        $("#picturess27").draggable();
+        $("#picturess28").draggable();
+        $("#picturess29").draggable();
+        $("#picturess30").draggable();
+        $("#picturess31").draggable();
+        $("#picturess32").draggable();
+        $("#picturess33").draggable();
+        $("#picturess34").draggable();
+        $("#picturess35").draggable();
+        $("#picturess36").draggable();
+
+
+    });
+
+
+    $("#newy").on('click', function() {
+        $('<img src="newyear/bal1.png" style="height:50px; width:50px; position:relative;" id="picturess37">').appendTo(".stickers");
+        $('<img src="newyear/cele.png" style="height:50px; width:50px; position:relative;" id="picturess38">').appendTo(".stickers");
+        $('<img src="newyear/celebrate.png" style="height:50px; width:50px; position:relative;" id="picturess39">').appendTo(".stickers");
+        $('<img src="newyear/cheer.png" style="height:50px; width:50px; position:relative;" id="picturess40">').appendTo(".stickers");
+        $('<img src="newyear/dance.png" style="height:50px; width:50px; position:relative;" id="picturess41">').appendTo(".stickers");
+        $('<img src="newyear/fire1.png" style="height:50px; width:50px; position:relative;" id="picturess42">').appendTo(".stickers");
+        $('<img src="newyear/fire3.png" style="height:50px; width:50px; position:relative;" id="picturess43">').appendTo(".stickers");
+        $('<img src="newyear/fire5.png" style="height:50px; width:50px; position:relative;" id="picturess44">').appendTo(".stickers");
+        $('<img src="newyear/firew.png" style="height:50px; width:50px; position:relative;" id="picturess45">').appendTo(".stickers");
+        $('<img src="newyear/firew2.png" style="height:50px; width:50px; position:relative;" id="picturess46">').appendTo(".stickers");
+        $('<img src="newyear/firew3.png" style="height:50px; width:50px; position:relative;" id="picturess47">').appendTo(".stickers");
+        $('<img src="newyear/firew6.png" style="height:50px; width:50px; position:relative;" id="picturess48">').appendTo(".stickers");
+        $('<img src="newyear/ny.png" style="height:50px; width:50px; position:relative;" id="picturess49">').appendTo(".stickers");
+        $('<img src="newyear/ny1.png" style="height:50px; width:50px; position:relative;" id="picturess50">').appendTo(".stickers");
+        $('<img src="newyear/ny3.png" style="height:50px; width:50px; position:relative;" id="picturess51">').appendTo(".stickers");
+        $('<img src="newyear/ny4.png" style="height:50px; width:50px; position:relative;" id="picturess52">').appendTo(".stickers");
+        $('<img src="newyear/newye.png" style="height:50px; width:50px; position:relative;" id="picturess53">').appendTo(".stickers");
+        $('<img src="newyear/hny.png" style="height:50px; width:50px; position:relative;" id="picturess54">').appendTo(".stickers");
+        $('<img src="newyear/dance3.png" style="height:50px; width:50px; position:relative;" id="picturess55">').appendTo(".stickers");
+        $('<img src="newyear/dance2.png" style="height:50px; width:50px; position:relative;" id="picturess56">').appendTo(".stickers");
+        $("#picturess37").draggable();
+        $("#picturess38").draggable();
+        $("#picturess39").draggable();
+        $("#picturess40").draggable();
+        $("#picturess41").draggable();
+        $("#picturess42").draggable();
+        $("#picturess43").draggable();
+        $("#picturess44").draggable();
+        $("#picturess45").draggable();
+        $("#picturess46").draggable();
+        $("#picturess47").draggable();
+        $("#picturess48").draggable();
+        $("#picturess49").draggable();
+        $("#picturess50").draggable();
+        $("#picturess51").draggable();
+        $("#picturess52").draggable();
+        $("#picturess53").draggable();
+        $("#picturess54").draggable();
+        $("#picturess55").draggable();
+        $("#picturess56").draggable();
+
+    });
+
+
+    $("#christ").on('click', function() {
+        $('<img src="xmas/bell.png" style="height:50px; width:50px; position:relative;" id="picturess57">').appendTo(".stickers");
+        $('<img src="xmas/candles.png" style="height:50px; width:50px; position:relative;" id="picturess58">').appendTo(".stickers");
+        $('<img src="xmas/decor.png" style="height:50px; width:50px; position:relative;" id="picturess59">').appendTo(".stickers");
+        $('<img src="xmas/decor1.png" style="height:50px; width:50px; position:relative;" id="picturess60">').appendTo(".stickers");
+        $('<img src="xmas/decor2.png" style="height:50px; width:50px; position:relative;" id="picturess61">').appendTo(".stickers");
+        $('<img src="xmas/decor3.png" style="height:50px; width:50px; position:relative;" id="picturess62">').appendTo(".stickers");
+        $('<img src="xmas/decor4.png" style="height:50px; width:50px; position:relative;" id="picturess63">').appendTo(".stickers");
+        $('<img src="xmas/decor6.png" style="height:50px; width:50px; position:relative;" id="picturess64">').appendTo(".stickers");
+        $('<img src="xmas/frame.png" style="height:50px; width:50px; position:relative;" id="picturess65">').appendTo(".stickers");
+        $('<img src="xmas/santa.png" style="height:50px; width:50px; position:relative;" id="picturess66">').appendTo(".stickers");
+        $('<img src="xmas/sign.png" style="height:50px; width:50px; position:relative;" id="picturess67">').appendTo(".stickers");
+        $('<img src="xmas/sign1.png" style="height:50px; width:50px; position:relative;" id="picturess68">').appendTo(".stickers");
+        $('<img src="xmas/snow.png" style="height:50px; width:50px; position:relative;" id="picturess69">').appendTo(".stickers");
+        $('<img src="xmas/snow2.png" style="height:50px; width:50px; position:relative;" id="picturess70">').appendTo(".stickers");
+        $('<img src="xmas/stocking.png" style="height:50px; width:50px; position:relative;" id="picturess71">').appendTo(".stickers");
+        $('<img src="xmas/tree.png" style="height:50px; width:50px; position:relative;" id="picturess72">').appendTo(".stickers");
+        $('<img src="xmas/xmas.png" style="height:50px; width:50px; position:relative;" id="picturess73">').appendTo(".stickers");
+        $('<img src="xmas/xmas2.png" style="height:50px; width:50px; position:relative;" id="picturess74">').appendTo(".stickers");
+        $('<img src="xmas/xmas3.png" style="height:50px; width:50px; position:relative;" id="picturess75">').appendTo(".stickers");
+        $('<img src="xmas/xmas5.png" style="height:50px; width:50px; position:relative;" id="picturess76">').appendTo(".stickers");
+        $("#picturess57").draggable();
+        $("#picturess58").draggable();
+        $("#picturess59").draggable();
+        $("#picturess60").draggable();
+        $("#picturess61").draggable();
+        $("#picturess62").draggable();
+        $("#picturess63").draggable();
+        $("#picturess64").draggable();
+        $("#picturess65").draggable();
+        $("#picturess66").draggable();
+        $("#picturess67").draggable();
+        $("#picturess68").draggable();
+        $("#picturess69").draggable();
+        $("#picturess70").draggable();
+        $("#picturess71").draggable();
+        $("#picturess72").draggable();
+        $("#picturess73").draggable();
+        $("#picturess74").draggable();
+        $("#picturess75").draggable();
+        $("#picturess76").draggable();
+
+    });
+
+
+    $("#vald").on('click', function() {
+        $('<img src="val/bemine.png" style="height:50px; width:50px; position:relative;" id="picturess77">').appendTo(".stickers");
+        $('<img src="val/bow.png" style="height:50px; width:50px; position:relative;" id="picturess78">').appendTo(".stickers");
+        $('<img src="val/choco.png" style="height:50px; width:50px; position:relative;" id="picturess79">').appendTo(".stickers");
+        $('<img src="val/cupid.png" style="height:50px; width:50px; position:relative;" id="picturess80">').appendTo(".stickers");
+        $('<img src="val/cupid1.png" style="height:50px; width:50px; position:relative;" id="picturess81">').appendTo(".stickers");
+        $('<img src="val/dino.png" style="height:50px; width:50px; position:relative;" id="picturess82">').appendTo(".stickers");
+        $('<img src="val/dove.png" style="height:50px; width:50px; position:relative;" id="picturess83">').appendTo(".stickers");
+        $('<img src="val/flowers.png" style="height:50px; width:50px; position:relative;" id="picturess84">').appendTo(".stickers");
+        $('<img src="val/frame.png" style="height:50px; width:50px; position:relative;" id="picturess85">').appendTo(".stickers");
+        $('<img src="val/gift.png" style="height:50px; width:50px; position:relative;" id="picturess86">').appendTo(".stickers");
+        $('<img src="val/heart.png" style="height:50px; width:50px; position:relative;" id="picturess87">').appendTo(".stickers");
+        $('<img src="val/hearts.png" style="height:50px; width:50px; position:relative;" id="picturess88">').appendTo(".stickers");
+        $('<img src="val/hearts1.png" style="height:50px; width:50px; position:relative;" id="picturess89">').appendTo(".stickers");
+        $('<img src="val/loveteddy.png" style="height:50px; width:50px; position:relative;" id="picturess90">').appendTo(".stickers");
+        $('<img src="val/rose.png" style="height:50px; width:50px; position:relative;" id="picturess91">').appendTo(".stickers");
+        $('<img src="val/teddy.png" style="height:50px; width:50px; position:relative;" id="picturess92">').appendTo(".stickers");
+        $('<img src="val/tree.png" style="height:50px; width:50px; position:relative;" id="picturess93">').appendTo(".stickers");
+        $('<img src="val/val1.png" style="height:50px; width:50px; position:relative;" id="picturess94">').appendTo(".stickers");
+        $('<img src="val/val3.png" style="height:50px; width:50px; position:relative;" id="picturess95">').appendTo(".stickers");
+        $('<img src="val/val5.png" style="height:50px; width:50px; position:relative;" id="picturess96">').appendTo(".stickers");
+        $('<img src="val/xoxo.png" style="height:50px; width:50px; position:relative;" id="picturess97">').appendTo(".stickers");
+        $("#picturess77").draggable();
+        $("#picturess78").draggable();
+        $("#picturess79").draggable();
+        $("#picturess80").draggable();
+        $("#picturess81").draggable();
+        $("#picturess82").draggable();
+        $("#picturess83").draggable();
+        $("#picturess84").draggable();
+        $("#picturess85").draggable();
+        $("#picturess86").draggable();
+        $("#picturess87").draggable();
+        $("#picturess88").draggable();
+        $("#picturess89").draggable();
+        $("#picturess90").draggable();
+        $("#picturess91").draggable();
+        $("#picturess92").draggable();
+        $("#picturess93").draggable();
+        $("#picturess94").draggable();
+        $("#picturess95").draggable();
+        $("#picturess96").draggable();
+        $("#picturess97").draggable();
+
+    });
+
+
 
     $('#black').on('click', function() {
         $('#card').css("background-color", "black");
@@ -130,10 +355,6 @@ $('document').ready(function() {
         $('#card').css("background-color", "KHAKI");
     });
 
+
+
 });
-//
-// $('button').on('click', function() {
-//   $('#textbox1').attr("disabled","disabled");
-//   $("#textbox1").draggable();
-//
-// });
